9c21d3ddd6c516dcd09973a5389f2216de0fd7f2
man_do <selvijaarmando@gmail.com>
2022-12-10 20:48:54 +0100

    move_group talks to arduino and gives command but prblms with the feedback

--- arduarm_hardware_interface/src/arduarm_hw_interface.cpp
+++ arduarm_hardware_interface/src/arduarm_hw_interface.cpp
@@ -1,39 +1,31 @@
 #include <arduarm_hardware_interface/arduarm_hw_interface.h>
-#include <std_msgs/Float64.h>
 #include <sensor_msgs/JointState.h>
 #include <cmath>
 
 namespace arduarm_hardware_interface
 {
 
-void ArduarmHWInterface::callback(const sensor_msgs::JointState::ConstPtr& msg){
-    for(int i = 0; i < num_joints_ ; ++i){
-      joint_position_[i] = msg->position[i] * M_PI / 180.0;
-  }
-}
+// void ArduarmHWInterface::callback(const sensor_msgs::JointState::ConstPtr& msg){
+//     for(int i = 0; i < num_joints_ ; ++i){
+//       joint_position_[i] = msg->position[i] * M_PI / 180.0;
+//   }
+// }
 
 ArduarmHWInterface::ArduarmHWInterface(ros::NodeHandle& nh, urdf::Model* urdf_model)
   :ros_control_boilerplate::GenericHWInterface(nh, urdf_model)
 {
   ROS_INFO_NAMED("arduarm_hw_interface", "ArduarmHWInterface Ready.");
   cmd_pub = nh_.advertise<sensor_msgs::JointState>("/arduarm/pos_cmd", 1000);
-  state_sub = nh_.subscribe("/arduarm/joint_pos", 1000, &ArduarmHWInterface::callback, this);
-
+  // state_sub = nh_.subscribe("/arduarm/joint_pos", 1000, &ArduarmHWInterface::callback, this);
   cmds.position.resize(3);
-
-  // cmds.layout.dim.push_back(std_msgs::MultiArrayDimension());
-  // cmds.layout.dim[0].label = "positions";
-  // cmds.layout.dim[0].size = 3;
-  // cmds.layout.dim[0].stride = 3;
-  // cmds.layout.data_offset = 0;
-
-  // states.layout.dim.push_back(std_msgs::MultiArrayDimension());
-  // states.layout.dim[0].label = "positions";
-  // states.layout.dim[0].size = 3;
 }
 
 void ArduarmHWInterface::read(ros::Duration& elapsed_time)
 {
+  //f fake feedback
+  for(int i = 0; i < num_joints_ ; ++i){
+      joint_position_[i] = joint_position_command_[i] * M_PI / 180.0;
+  }
 }
 
 void ArduarmHWInterface::write(ros::Duration& elapsed_time)
@@ -41,14 +33,10 @@ void ArduarmHWInterface::write(ros::Duration& elapsed_time)
   // Safety
   enforceLimits(elapsed_time);
 
-  // std::vector<int> vec(num_joints_);
-
   for (int i = 0; i < num_joints_; ++i){
     int temp = (int)(joint_position_command_[i] * 180.0 / M_PI);
     cmds.position[i] = temp;
   }
-  // cmds.position = vec;
-
   cmd_pub.publish(cmds);
 }
 
--- arduarm_hardware_interface/src/arduino/arduino.ino
+++ arduarm_hardware_interface/src/arduino/arduino.ino
@@ -1,34 +1,69 @@
-#if defined(ARDUINO) && ARDUINO >= 100
-#include "Arduino.h"
-#else
-#include <WProgram.h>
-#endif
-
+#include <Arduino.h>
 #include <Servo.h>
 #include <ros.h>
 #include <std_msgs/UInt16.h>
 #include <sensor_msgs/JointState.h>
 
+//#define servo_delay 50
+#define START 550
+#define END 2400
 ros::NodeHandle_<ArduinoHardware, 2, 2, 80, 105> nh;
-
 Servo servos[3];
 
 byte num_joints = 3;
-double pins[] = {9, 10, 11};
-double cmds[] = {0, 0, 0};
-double prev[] = {0, 0, 0};
-//int8_t initial[] = {90, 90, 90};
-
+float pins[] = {9, 10, 11};
+float cmds[] = {0, 0, 0};
+float prev[] = {0, 0, 0};
 byte positionChanged = 0;
+
+int sgn(int x) {
+  if (x > 0) return 1;
+  if (x < 0) return -1;
+  return 0;
+}
+
+int toMicros(int angle) {
+  //angle = map(angle, -90, 90, 0, 180);
+  return map(angle, 0, 180, START, END);
+}
+
+void cmd_all(int s1, int s2, int s3) {
+  int current[3];
+  int goal[3] = {(int)s1, (int)s2, (int)s3};
+  int diff[3];
+
+  while (*current != *goal) {
+    current[0] = servos[0].read();
+    current[1] = servos[1].read();
+    current[2] = servos[2].read();
+    diff[0] = goal[0] - current[0];
+    diff[1] = goal[1] - current[1];
+    diff[2] = goal[2] - current[2];
+    // convert to millis
+    servos[0].writeMicroseconds(toMicros(current[0] += sgn(diff[0])));
+
+    delay(20);
+
+    servos[1].writeMicroseconds(toMicros(current[1] += sgn(diff[1])));
+
+    delay(20);
+
+    servos[2].writeMicroseconds(toMicros(current[2] += sgn(diff[2])));
+
+    delay(5);
+  }
+}
 
 void servo_cb(const sensor_msgs::JointState& cmd_msg) {
   cmds[0] = radiansToDegrees(cmd_msg.position[0]);
   cmds[1] = radiansToDegrees(cmd_msg.position[1]);
   cmds[2] = radiansToDegrees(cmd_msg.position[2]);
 
-  for (int i = 0; i < num_joints; ++i) {
-    servos[i].write(cmds[i]);
-  }
+  //  for (int i = 0; i < num_joints; ++i) {
+  //    servos[i].write(cmds[i]);
+  //  }
+
+  cmd_all(cmds[0], cmds[1], cmds[2]);
 
   if (prev[0] == cmds[0] && prev[1] == cmds[1] && prev[2] == cmds[2] && positionChanged == 0)
   {
@@ -45,7 +80,7 @@ void servo_cb(const sensor_msgs::JointState& cmd_msg) {
 }
 
 
-ros::Subscriber<sensor_msgs::JointState> sub("pos_cmd", servo_cb);
+ros::Subscriber<sensor_msgs::JointState> sub("/arduarm/pos_cmd", servo_cb);
 
 void setup() {
   nh.getHardware()->setBaud(115200);
@@ -57,9 +92,10 @@ void setup() {
   servos[2].attach(pins[2]);
 
   delay(1);
-  servos[0].write(90);
-  servos[1].write(90);
-  servos[2].write(90);
+//  servos[0].write(90);
+//  servos[1].write(90);
+//  servos[2].write(90);
+  cmd_all(90, 90, 90);
 }
 
 void loop() {
